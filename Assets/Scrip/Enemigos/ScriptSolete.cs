8d861d1aa5f8a19638b0adbff4458dc22dda7b96
joselu <56913129+JosBY64@users.noreply.github.com>
2021-01-31 02:52:23 +0100

    Empiezo a programar mami

---
+++ Assets/Scrip/Enemigos/ScriptSolete.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class ScriptSolete : MonoBehaviour
+{
+    Transform _player;
+    NavMeshAgent _navM;
+    Collider2D _myColli;
+
+    [SerializeField]
+    GameObject pato1, pato2;
+
+    float x;
+    float y;
+
+    [SerializeField]
+    float _daño;
+
+    [SerializeField]
+    float _speed;
+
+    [SerializeField]
+    bool sePuedeRomper;
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _myColli = GetComponent<Collider2D>();
+        _navM = GetComponent<NavMeshAgent>();
+        _navM.updateRotation = false;
+        _navM.updateUpAxis = false;
+    }
+
+
+    void FixedUpdate()
+    {
+        float distanciaObjetivo = Vector3.Distance(transform.position, _player.transform.position);
+        float distanciaEnX = (transform.position.x - _player.transform.position.x);
+        float distanciaEnY = (transform.position.y - _player.transform.position.y);
+
+        Debug.Log("distancia en x : " + distanciaEnX + "distancia en y : " + distanciaEnY);
+
+
+        if (distanciaObjetivo > 0.5)
+        {
+            transform.position = Vector3.Lerp(transform.position, _player.transform.position, _speed * Time.deltaTime);
+        }
+        else
+        {
+            
+        }
+
+    }
+}
