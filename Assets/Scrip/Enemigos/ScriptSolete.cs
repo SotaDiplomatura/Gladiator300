dc3c9ce76a971be1184dda94077867ca42c4d894
joselu <56913129+JosBY64@users.noreply.github.com>
2021-01-31 03:21:04 +0100

    solardo

--- Assets/Scrip/Enemigos/ScriptSolete.cs
+++ Assets/Scrip/Enemigos/ScriptSolete.cs
@@ -40,16 +40,24 @@ public class ScriptSolete : MonoBehaviour
         float distanciaEnX = (transform.position.x - _player.transform.position.x);
         float distanciaEnY = (transform.position.y - _player.transform.position.y);
 
-        Debug.Log("distancia en x : " + distanciaEnX + "distancia en y : " + distanciaEnY);
+        //Debug.Log("distancia en x : " + distanciaEnX + "distancia en y : " + distanciaEnY);
 
 
-        if (distanciaObjetivo > 0.5)
+        if (distanciaObjetivo > 3)
         {
             transform.position = Vector3.Lerp(transform.position, _player.transform.position, _speed * Time.deltaTime);
         }
         else
         {
-            
+
+            if (distanciaEnY < distanciaEnX)
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, _player.transform.position.y, 0), _speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(_player.transform.position.x, transform.position.y, 0), _speed * Time.deltaTime);
+            }
         }
 
     }
